be7ba2ad5047250114137e7dd061811cdc81c2e5
MauWebs <mauriciovidalwebs@gmail.com>
2026-03-12 12:27:32 -0300

    feat: link of whatsapp

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -51,13 +51,13 @@ export default function Footer() {
           </p>
 
           <ul role="list" className="flex gap-5 text-white/40">
-            <li>
+            {/* <li>
               <a aria-label="X" className="block hover:text-white/80 transition-colors">
                 <svg viewBox="0 0 16 16" aria-hidden="true" className="h-4 w-4">
                   <path fill="currentColor" d="M12.6 0h2.454l-5.36 6.778L16 16h-4.937l-3.867-5.594L2.771 16H.316l5.733-7.25L0 0h5.063l3.495 5.114L12.601 0Zm-.86 14.376h1.36L4.323 1.539H2.865l8.875 12.837Z"></path>
                 </svg>
               </a>
-            </li>
+            </li> */}
             <li>
               <a aria-label="Instagram" href="https://www.instagram.com/opthana_es/" target="_blank" rel="noopener noreferrer" className="block hover:text-[#c13584] transition-colors">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
@@ -66,7 +66,7 @@ export default function Footer() {
               </a>
             </li>
             <li>
-              <a aria-label="WhatsApp" className="block hover:text-[#25D366]/95 transition-colors" href="">
+              <a aria-label="WhatsApp" className="block hover:text-[#25D366]/95 transition-colors" href="https://wa.me/549223539559?text=Hola,%20me%20interesaría%20realizar%20un%20desarrollo%20o%20plataforma!">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                   <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
                 </svg>
